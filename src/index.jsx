66a20704e3f12897cb33babce5167cac82631b9d
Bin Huang <huangbin88@foxmail.com>
2015-12-03 14:11:29 +0800

    Add redux-devtools

--- src/index.jsx
+++ src/index.jsx
@@ -5,15 +5,23 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import createRoutes from './route/createRoutes';
 import configureStore from './store/configureStore';
 
 const history = createBrowserHistory();
 const routes = createRoutes();
+const store = configureStore();
 
 render((
-  <Provider store={configureStore()}>
-    <Router history={history} routes={routes} />
-  </Provider>
+  <div>
+    <Provider store={store}>
+      <Router history={history} routes={routes} />
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>
 ), document.getElementById('container'));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,19 @@
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import { devTools, persistState } from 'redux-devtools';
+
 import rootReducer from '../reducers';
 
 const configureStore = () => {
-  const store = createStore(rootReducer);
+  const finalCreateStore = compose(
+    // Enables your middleware:
+    // applyMiddleware(m1, m2, m3), // any Redux middleware, e.g. redux-thunk
+
+    devTools(),
+
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+  )(createStore);
+
+  const store = finalCreateStore(rootReducer);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
